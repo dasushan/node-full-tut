fc9ba42777c0f7e67df45929c23c4f3f81e4305b
dasushan <53913896+dasushan@users.noreply.github.com>
2023-09-12 10:47:27 +0530

    ...

--- app.js
+++ app.js
@@ -20,5 +20,7 @@ const items = [1, [2, [3, [4]]]]
 const newItems = _.flattenDeep(items)
 console.log(newItems)
 
+ console.log("hello world")
+
 // npm install  -- command installs all packages that are present in dependencies of the package.json file
 // lodash is a utility library
