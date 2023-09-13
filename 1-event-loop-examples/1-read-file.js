a62958adaea7e28f8785e1d0b65ccbc3e1c6ef7e
dasushan <53913896+dasushan@users.noreply.github.com>
2023-09-13 11:16:42 +0530

    Purpose: adding new files
    Description:
    event-loop examples

---
+++ 1-event-loop-examples/1-read-file.js
@@ -0,0 +1,15 @@
+const fs = require('fs')
+
+console.log('started a first task')
+// CHECK FILE PATH !!!
+fs.readFile('./content/first.txt', 'utf8', (err, result) => {
+    if (err) {
+        console.log(err)
+        return
+    }
+    console.log(result)
+    console.log('completed first task')
+})
+console.log('starting next task')
+
+
