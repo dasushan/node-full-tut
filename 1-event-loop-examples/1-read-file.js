8bb401723d7bc2cd9085cc5e6a78fe0f0e1e9d95
dasushan <53913896+dasushan@users.noreply.github.com>
2023-09-13 15:20:50 +0530

    ..

--- 1-event-loop-examples/1-read-file.js
+++ 1-event-loop-examples/1-read-file.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 
 console.log('started a first task')
 // CHECK FILE PATH !!!
-fs.readFile('./content/first.txt', 'utf8', (err, result) => {
+fs.readFile('../content/first.txt', 'utf8', (err, result) => {
     if (err) {
         console.log(err)
         return
