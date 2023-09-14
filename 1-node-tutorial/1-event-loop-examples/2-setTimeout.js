de69bf6685cacb82c2d20b7f9c5c12116291b171
dasushan <53913896+dasushan@users.noreply.github.com>
2023-09-14 13:40:54 +0530

    restructuring

---
+++ 1-node-tutorial/1-event-loop-examples/2-setTimeout.js
@@ -0,0 +1,7 @@
+// started operaring system process
+console.log('first')
+setTimeout(() => {
+    console.log('second')
+}, 0)
+console.log('third')
+// completed and exited operatinf system process
